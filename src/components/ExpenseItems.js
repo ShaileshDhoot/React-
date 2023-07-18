05f2a18ec3dd8a97bc3d5d320f594c3a3acbbdb7
ShaileshDhoot <shaileshdhoot25@gmail.com>
2023-07-18 22:09:53 +0530

    using prrops

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,10 +1,21 @@
-function ExpenseItems (){
+import "./ExpenseItems.css"
+
+function ExpenseItems (props){
+    const { title, amount, date, location } = props;
+    const month = date.getMonth()+1
+    const day = date.getDate()
+    const year = date.getFullYear()
     return (
     <div className="expense-item"> 
-        <div>18 july 2023</div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+        <div>{location}</div> 
         <div className="expense-item__description">
-            <h2>Car Insurance</h2>
-            <div className="expense-item__price">8500</div>             
+            <h2>{title}</h2>
+           
+            <div className="expense-item__price">{amount}</div> 
+                       
         </div>
    </div>
    )
