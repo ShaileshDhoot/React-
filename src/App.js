b806319693cdec412044943a9463d9726aa5a8e1
ShaileshDhoot <shaileshdhoot25@gmail.com>
2023-08-01 19:30:26 +0530

    expense form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 
 import Expenses from "./components/Expense/Expenses";
-
+import NewExpense from "./components/NewExpense/NewExpense";
 const App = () => {
  
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense/>
       <Expenses/>
 
     </div>
--- src/components/Expense/ExpenseItems.js
+++ src/components/Expense/ExpenseItems.js
@@ -1,13 +1,20 @@
 import ExpenseDate from "./ExpenseDate";
+import React , { useState } from "react"
 import ExpenseDetails from "./ExpenseDetails";
 import Expenses from "./Expenses";
 import Card from "../UI/Card";
 import "./ExpenseItems.css"
 
 const ExpenseItems = (props) =>{
-    const { title, amount, date, location } = props;
+
+
+    const [title, newTitle] = useState(props.title)
+
+    const {  amount, date, location } = props;
+
     const clickHandler=()=>{
-        console.log('buton clicked !')
+        newTitle('Updated')
+        console.log(title)
     }
     const deleteHandler = ()=>{
         console.log(typeof(Expenses))
