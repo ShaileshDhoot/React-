2e8c44c4353fc2ea048642c381c4b807c0494ce4
ShaileshDhoot <shaileshdhoot25@gmail.com>
2023-08-01 19:47:19 +0530

    onChange eventlistner for input values

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,19 +1,32 @@
-import React from "react"
+import React , {useState} from "react"
 import './ExpenseForm.css'
-const ExpenseForm = ()=>{
+const ExpenseForm = ()=>{ 
+    const [enteredTitle, setEnteredTitle] = useState('')
+    const [eneteredDate, setEnteredDate] = useState('')
+    const [eneteredAmount, setEnteredAmount] = useState('')
+    const titleChangeHandler = (event)=>{
+        setEnteredTitle(event.target.value)
+    }
+    const dateChangeHandler = (event)=>{
+        setEnteredDate(event.target.value)
+    }
+    const amountChangeHandler = (event)=>{
+        setEnteredAmount(event.target.value)
+    }
+
     return <form>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Expense Details</label>
-                    <input type="text"/>
+                    <input type="text" onChange={titleChangeHandler}/>
                 </div>
                 <div className="new-expense__control">
                     <label>Expense Date</label>
-                    <input type="date"/>
+                    <input type="date" onChange={dateChangeHandler}/>
                 </div>
                 <div className="new-expense__control">
                     <label>Expense Amount</label>
-                    <input type="number"/>
+                    <input type="number" onChange={amountChangeHandler}/>
                 </div>
                 <div className="new-expense__actions">
                     <button>Add Expense</button>
