0d96085686d49118cd742f1b296b97b31bf1deb8
ShaileshDhoot <shaileshdhoot25@gmail.com>
2023-08-02 19:37:26 +0530

    submit form data

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,20 +1,56 @@
 import React , {useState} from "react"
 import './ExpenseForm.css'
 const ExpenseForm = ()=>{ 
+
     const [enteredTitle, setEnteredTitle] = useState('')
     const [eneteredDate, setEnteredDate] = useState('')
     const [eneteredAmount, setEnteredAmount] = useState('')
+
+    // const  [userInput, setUserInput] = useState({
+    //     enteredTitle: '',
+    //     eneteredDate: '',
+    //     eneteredAmount: '',
+    // })
+    // usrin spread operator because to copy the values 
     const titleChangeHandler = (event)=>{
-        setEnteredTitle(event.target.value)
+            setEnteredTitle(event.target.value)
+        // setUserInput({
+        //     ...userInput,
+        //     enteredTitle: event.target.value
+        // })
+
+        // this method gurantees that the previous state is the latest state , but not in above case
+        // setUserInput((prevState)=>{
+        //     return { ...prevState,enteredTitle: event.target.value }
+        // })
     }
     const dateChangeHandler = (event)=>{
-        setEnteredDate(event.target.value)
+            setEnteredDate(event.target.value)
+        // setUserInput({
+        //     ...userInput,
+        //     eneteredDate: event.target.value
+        // })
     }
     const amountChangeHandler = (event)=>{
-        setEnteredAmount(event.target.value)
+            setEnteredAmount(event.target.value)
+        // setUserInput({
+        //     ...userInput,
+        //     eneteredAmount: event.target.value
+        // })
     }
 
-    return <form>
+    const sumitHandler = (e) =>{
+        e.preventDefault();
+        const expenseData = {
+            title: enteredTitle,
+            date: new Date(eneteredDate),
+            amount: eneteredAmount,
+        }
+        console.log(expenseData)
+
+    }
+
+    return <form onSubmit={sumitHandler}>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Expense Details</label>
@@ -29,7 +65,7 @@ const ExpenseForm = ()=>{
                     <input type="number" onChange={amountChangeHandler}/>
                 </div>
                 <div className="new-expense__actions">
-                    <button>Add Expense</button>
+                    <button type="submit">Add Expense</button>
                 </div>
             </div>
 
