439bacae20618169c3dc787ced471a19465c6fa7
ShaileshDhoot <shaileshdhoot25@gmail.com>
2023-08-08 04:21:46 +0530

    Rendering Expenses with performance optimisations

--- src/components/Expense/ExpenseFilter.js
+++ src/components/Expense/ExpenseFilter.js
@@ -8,6 +8,7 @@ const ExpensesFilter = () => {
       <div className='expenses-filter__control'>
         <label>Filter by year</label>
         <select>
+        <option value='2023'>2023</option>
           <option value='2022'>2022</option>
           <option value='2021'>2021</option>
           <option value='2020'>2020</option>
